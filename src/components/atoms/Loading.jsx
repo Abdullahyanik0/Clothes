e75141824e8860ac2610ea09c47aa7a08f9b3e11
Abdullahyanik0 <abdullahyanik016@gmail.com>
2022-10-06 01:19:18 +0300

    basket removeButton and bug fix

--- src/components/atoms/Loading.jsx
+++ src/components/atoms/Loading.jsx
@@ -4,7 +4,7 @@ import { CircularProgress } from "@mui/material";
 export default function LinearIndeterminate() {
   return (
     <div className="flex justify-center  items-center ">
-      <CircularProgress size={100} />
+      <CircularProgress size={100} /> 
     </div>
   );
 }
